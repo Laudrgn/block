1420c76df0fb752b0e7667d39e06eb72152a8f89
Laudrgn <112733599+Laudrgn@users.noreply.github.com>
2022-09-24 11:22:19 -0300

    Update settings.py

--- miblog/settings.py
+++ miblog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ip&8u98kb-jg_22+fr0lav$4d$ok#=w-#-6wpwrbop1rxx9wsv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
